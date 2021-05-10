3b3371aa2dc56c0cb28bbc2f04d62c0e38d743dd
Bastiano Vitali <bvitali@fnal.gov>
2021-05-10 04:31:01 -0500

    just before MR

--- RecoDataProducts/inc/LumiInfo.hh
+++ RecoDataProducts/inc/LumiInfo.hh
@@ -4,13 +4,13 @@
 #include <math.h>
 //
 // This object is a mock-up to contain the variables to monitor the luminosity
-// The exact structure is still under discussion. (now just an unsigned short [2])
+// The exact structure is still under discussion. (now just an unsigned short [5])
 //
-// To save space we might aim to have a unit16_t * lumi = new unit16_t
+// To save space we might aim to have a unit16_t * lumi = new unit16_t; with
 // lumi[0] : 8 bit for the version, 8 bit for the number of entries
 // lumi[i] : 16 bit to indicate the i-number for the lumi (proton TC, calo energy etc)
 //
-// bvitali Apr 2021
+// bvitali May 2021
 
 
 namespace mu2e {
@@ -20,9 +20,12 @@ namespace mu2e {
     LumiInfo(){
       _lumi[0] = 0;
       _lumi[1] = 0;
+      _lumi[2] = 0;
+      _lumi[3] = 0;
+      _lumi[4] = 0;
     }    
 
-    unsigned short _lumi[2];
+    unsigned short _lumi[5];
 
   };
 }
